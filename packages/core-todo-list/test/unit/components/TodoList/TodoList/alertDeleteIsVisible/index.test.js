7549632f58762d60e46805f939636e3e0f6fcdbc
Sidani, Mohammad | Sippi | LPD <mohammad.sidani@rakuten.com>
2023-07-26 23:20:32 +0900

    test: add new tests for todo list

---
+++ packages/core-todo-list/test/unit/components/TodoList/TodoList/alertDeleteIsVisible/index.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { render, screen, fireEvent, act } from '@testing-library/react-native'
+import { Provider } from 'react-redux'
+import Component from '../../../../../../src/components/TodoList'
+import configureStore from 'redux-mock-store'
+import { Alert } from 'react-native'
+import thunk from 'redux-thunk'
+import todoListItemSelectors from '../../../../../lib/selector/todoListItem'
+
+const mockStore = configureStore([thunk])
+
+const store = mockStore({
+  todoList: {
+    list: new Array(10).fill({}).map((_, index) => ({ task: 't'.repeat(10), id: `${index}` }))
+  },
+  auth: {
+    user: {
+      isLoggedIn: true
+    }
+  }
+})
+
+jest.mock('expo-local-authentication', () => ({
+  authenticateAsync: jest.fn(() => ({ success: true }))
+}))
+
+describe('TodoList', () => {
+  test('Delete alert is visible with login success', async () => {
+    const alertSpy = jest.spyOn(Alert, 'alert')
+    render(<Provider store={store}><Component /></Provider>).toJSON()
+    const checkboxList = await screen.findAllByTestId(todoListItemSelectors.checkbox)
+    const checkbox = checkboxList[0]
+    await act(() => {
+      fireEvent.press(checkbox)
+    })
+    expect(alertSpy).toHaveBeenCalledWith(
+      'Delete task',
+      'Task: tttttttttt',
+      [
+        { onPress: expect.any(Function), style: 'destructive', text: 'Delete' },
+        { onPress: expect.any(Function), style: 'cancel', text: 'Cancel' }
+      ],
+      { cancelable: true, onDismiss: expect.any(Function) })
+  })
+})
---
+++ packages/core-todo-list/test/unit/components/TodoList/TodoList/alertErrorIsVisible/index.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { render, screen, fireEvent, act } from '@testing-library/react-native'
+import { Provider } from 'react-redux'
+import Component from '../../../../../../src/components/TodoList'
+import configureStore from 'redux-mock-store'
+import { Alert } from 'react-native'
+import thunk from 'redux-thunk'
+import todoListItemSelectors from '../../../../../lib/selector/todoListItem'
+
+const mockStore = configureStore([thunk])
+
+const store = mockStore({
+  todoList: {
+    list: new Array(10).fill({}).map((_, index) => ({ task: 't'.repeat(10), id: `${index}` }))
+  },
+  auth: {
+    user: {
+      isLoggedIn: true
+    }
+  }
+})
+
+jest.mock('expo-local-authentication', () => ({
+  authenticateAsync: jest.fn(() => ({ error: '__error__' }))
+}))
+
+describe('TodoList', () => {
+  test('Error alert is visible with login success', async () => {
+    const alertSpy = jest.spyOn(Alert, 'alert')
+    render(<Provider store={store}><Component /></Provider>).toJSON()
+    const todoListItems = await screen.findAllByTestId(todoListItemSelectors.root)
+    const todoListItem = todoListItems[0]
+    await act(() => {
+      fireEvent.press(todoListItem)
+    })
+    expect(alertSpy).toHaveBeenCalledWith(
+      'Something went wrong!',
+      '__error__',
+      [
+        { onPress: expect.any(Function), style: 'cancel', text: 'Cancel' }
+      ],
+      { cancelable: true, onDismiss: expect.any(Function) })
+  })
+})
